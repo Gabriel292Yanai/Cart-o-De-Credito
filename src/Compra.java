2c64838b59e54af69168ac91b45578b4f61c3461
Gabriel Yanai <gkyanai292@gmail.com>
2025-04-29 12:53:35 -0300

    Add files via upload

---
+++ src/Compra.java
@@ -0,0 +1,29 @@
+public class Compra implements Comparable<Compra>{
+    private String descricao;
+    private double valor;
+
+    public Compra(String descricao, double valor) {
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Compra: descricao = " + descricao +
+                " valor=" + valor;
+    }
+
+    //vai pegar esse valor e comparar com o valor da outra compra
+    @Override
+    public int compareTo(Compra outraCompra) {
+        return Double.valueOf(this.valor).compareTo(Double.valueOf(outraCompra.valor));
+    }
+}
